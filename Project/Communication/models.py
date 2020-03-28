4dfcf9c4e8666bffd5f602a24ce825f2722192c0
drew <drew.design@mail.ru>
2020-03-28 09:58:48 +0300

    [ Adding msg form, views for follow, other pages edits ]

--- Project/Communication/models.py
+++ Project/Communication/models.py
@@ -9,6 +9,6 @@ class Message(models.Model):
     time = models.DateTimeField(auto_now_add=True)
 
 class Dialog(models.Model):
-    connected_to = models.ForeignKey('ExtendsUserModel.User', on_delete=models.CASCADE, related_name='dialog_connected_to', null=False)
-    dialog_with = models.ForeignKey('ExtendsUserModel.User', on_delete=models.CASCADE, related_name='dialog_with', null=False)
+    connected_to = models.ForeignKey('ExtendsUserModel.User', on_delete=models.CASCADE, related_name="con_to", null=True)
+    dialog_with = models.ForeignKey('ExtendsUserModel.User', on_delete=models.CASCADE, related_name="dial_with", null=True)
     messages = models.ManyToManyField('Communication.Message')
--- Project/ExtendsUserModel/urls.py
+++ Project/ExtendsUserModel/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.friends, name='friends'),
+    path('accept/<int:pk>/<str:page>/', views.accept, name='accept'),
+    path('follow/<int:pk>/', views.follow, name='follow'),
+    path('unfollow/<int:pk>/<str:page>/', views.unfollow, name='unfollow'),
+    path('unfriend/<int:pk>/<str:page>/', views.unfriend, name='unfriend'),
 ]
